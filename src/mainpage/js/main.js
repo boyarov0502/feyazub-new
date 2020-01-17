9c1d4d985b3b0bde16bb92a11815b4b244aa95a2
AinoGG <ryadomgg@gmail.com>
2020-01-17 18:10:03 +0300

    add slider

--- src/mainpage/js/main.js
+++ src/mainpage/js/main.js
@@ -3,4 +3,25 @@ $(document).ready(function () {
 		arrows: false,
 		dots: true
 	});
+
+
+	$('.fear__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: false,
+		asNavFor: '.slider-nav'
+	  });
+	  $('.slider-nav').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.fear__slider',
+		dots: false,
+		centerMode: true,
+		focusOnSelect: true,
+		centerPadding: '0px',
+		centerMode: true
+	  });
+
 });
+
