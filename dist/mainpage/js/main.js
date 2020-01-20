702ff820ff01113f9024ac57058f650627f34b13
boyarov0502 <poluboiarov@gmail.com>
2020-01-20 18:59:35 +0500

    fancy optimization

--- dist/mainpage/js/main.js
+++ dist/mainpage/js/main.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
 		arrows: false,
 		dots: true
 	});
+
+	
+
 });
+
+
+
+  $(".fear-img")
+    .attr('rel', 'gallery')
+    .fancybox({
+      padding : 0,
+      helpers : {
+        overlay : {
+          css : {
+            'background' : 'rgba(58, 42, 45, 0.95)'
+          }
+        }
+      }
+	});
+	
+	
